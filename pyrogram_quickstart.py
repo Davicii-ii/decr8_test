02515a9a99f72a908066d6cfc8ae6eeb513fd100
notImportanat <aviciishawn@gmail.com>
2021-02-10 20:09:35 +0300

    Update pyrogram_quickstart.py

--- pyrogram_quickstart.py
+++ pyrogram_quickstart.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 app = Client("decr8")
 
-@app.on_message(filters.private)
+@app.on_message()
 async def hello(client, message):
     await message.reply_text(f"Hello {message.from_user.mention}")
 
